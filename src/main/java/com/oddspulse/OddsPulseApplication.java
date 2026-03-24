7704bd9644f7203372d0c8e901cb1fe7b1b50e21
ChiraCosminFlorian <chiracosminflorian@gmail.com>
2026-03-24 19:00:55 +0200

    chore: initialize Spring Boot project with base dependencies

---
+++ src/main/java/com/oddspulse/OddsPulseApplication.java
@@ -0,0 +1,12 @@
+package com.oddspulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddsPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddsPulseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oddspulse/OddsPulseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.oddspulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddsPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
